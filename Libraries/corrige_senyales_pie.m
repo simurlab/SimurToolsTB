02a5ac5090b223df3784213bf149a507c3ccbc38
Diego Álvarez <dalvarez@uniovi.es>
2025-05-05 14:45:06 +0200

    Corregido corrige_senyales_pie

--- Libraries/corrige_senyales_pie.m
+++ Libraries/corrige_senyales_pie.m
@@ -4,7 +4,7 @@
 % y sustitucion por "valores razonables".
 %
 
-function [gyroml, gyroap]=corrige_senyales_pie(gyroml, gyroap, freq)
+function [gyroml, gyroap]=corrige_senyales_pie(gyroml, gyroap)
 % ***********************************************************************
 % ********** DETECCIÓN Y ELIMINACIÓN DE VALORES NAN DE gyroml ***********
 % En el vector informacion_NaN almacenamos:
@@ -14,13 +14,58 @@ function [gyroml, gyroap]=corrige_senyales_pie(gyroml, gyroap, freq)
 % (variable indices_NaN_gyroml).
 indices_NaN_gyroml = find(isnan(gyroml));
 cantidad_NaN_gyroml = length(indices_NaN_gyroml);
-informacion_NaN_gyroml = [cantidad_NaN_gyroml, indices_NaN_gyroml];
-gyroml = gyroml(~isnan(gyroml)); % Eliminación de todos los valores NaN
+informacion_NaN_gyroml = [cantidad_NaN_gyroml, indices_NaN_gyroml]; %#ok<NASGU>
+
+%En caso de que el primer o el último valor sean NaN los reemplazamos por 0
+if isnan(gyroml(1))
+    gyroml(1)=0;
+    indices_NaN_gyroml=indices_NaN_gyroml(2:end);
+end
+if isnan(gyroml(end))
+    gyroml(end)=0;
+    indices_NaN_gyroml=indices_NaN_gyroml(1:end-1);
+end
+% Reemplazamos los valores de NaN por una interpolación del anterior y
+%posterior
+for indice=indices_NaN_gyroml
+    desplaza=1;
+    while isnan(gyroml(indice+desplaza))
+        desplaza=desplaza+1;
+    end
+    salto=gyroml(indice+desplaza)-gyroml(indice-1);
+    paso=salto/(desplaza+1);
+    gyroml(indice)=gyroml(indice-1)+paso;
+end
+
 %
 indices_NaN_gyroant = find(isnan(gyroap));
 cantidad_NaN_gyroant = length(indices_NaN_gyroant);
-informacion_NaN_gyroant = [cantidad_NaN_gyroant, indices_NaN_gyroant];
-gyroap = gyroap(~isnan(gyroap)); % Eliminación de todos los valores NaN
+informacion_NaN_gyroant = [cantidad_NaN_gyroant, indices_NaN_gyroant]; %#ok<NASGU>
+
+%En caso de que el primer o el último valor sean NaN los reemplazamos por 0
+if isnan(gyroap(1))
+    gyroap(1)=0;
+    indices_NaN_gyroant=indices_NaN_gyroant(2:end);
+end
+if isnan(gyroap(end))
+    gyroap(end)=0;
+    indices_NaN_gyroant=indices_NaN_gyroant(1:end-1);
+end
+% Reemplazamos los valores de NaN por una interpolación del anterior y
+%posterior
+for indice=indices_NaN_gyroant
+    desplaza=1;
+    while isnan(gyroap(indice+desplaza))
+        desplaza=desplaza+1;
+    end
+    salto=gyroap(indice+desplaza)-gyroap(indice-1);
+    paso=salto/(desplaza+1);
+    gyroap(indice)=gyroap(indice-1)+paso;
+end
+
+
+
+
 % ***********************************************************************
 
 %% Medidas de la calidad en señales y detecciones crudas:
