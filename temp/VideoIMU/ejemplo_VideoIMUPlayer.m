c9f3e05a36a268dfa46076632497ca228d84a9b9
54Y37H <124357562+54Y37H@users.noreply.github.com>
2026-07-16 16:09:57 +0200

    Update ejemplo_VideoIMUPlayer.m
    Ejemplo con datos de dynathlon

--- temp/VideoIMU/ejemplo_VideoIMUPlayer.m
+++ temp/VideoIMU/ejemplo_VideoIMUPlayer.m
@@ -20,3 +20,37 @@ p = VideoIMUPlayer(ruta_video, db_file, id_sensor, seniales2plot);
     % lupa o el simbolo desplazamiento.
 
 %p.plotSyncError();
+
+
+%% Ejemplo con directos de Dynathlon
+% ruta_DynathlonData = "C:\Users\sayet\Downloads\Telegram Desktop\experiment_20260416_113857";
+% 
+% % Localizar video
+% fileMP4 = dir(fullfile(ruta_DynathlonData, '*.mp4'));
+% if numel(fileMP4) > 1, error("Debe haber un único video en: \n\t %s",ruta_DynathlonData); end
+% if numel(fileMP4) < 1, error("Video no encontrado en: \n\t %s",ruta_DynathlonData); end
+% 
+% rutaCompletaMP4 = string(fullfile(fileMP4.folder, fileMP4.name));   % *** input con ruta para VideoIMUPlayer debe ser STRING 
+% 
+% %crear_FramesFolder(rutaCompletaMP4);
+% 
+% % Localizar .mat
+% fileMAT = dir(fullfile(ruta_DynathlonData, '*.mat'));
+% if numel(fileMAT) > 1, error("Debe haber un único .mat en: \n\t %s",ruta_DynathlonData); end
+% if numel(fileMAT) < 1
+%     disp('.mat no encontrado, creando ...')
+%     d = Dynathlon('ruta_carpeta', ruta_DynathlonData, 'save', 'y', 'actividad', 'Vallas', 'freq', 120);
+%     fileMAT = dir(fullfile(ruta_DynathlonData, '*.mat'));
+% end
+% 
+% db_file = fullfile(fileMAT.folder, fileMAT.name);
+% 
+% % Nombre del sensor
+% id_sensor = "MD";                                                   % *** Debe ser STRING
+% 
+% % Señales a mostrar:
+% %seniales2plot = ["Gyr_X","Gyr_Y","Gyr_Z","Acc_Mod"];
+% %p = VideoIMUPlayer(rutaCompletaMP4, db_file, id_sensor, seniales2plot);
+% p = VideoIMUPlayer(rutaCompletaMP4, db_file, id_sensor);
+% 
+% p.plotSyncError();
